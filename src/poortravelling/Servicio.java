dddce0a34cf0ba184fa479b21604f32ab2d55baa
silva <silva@LAPTOP-51IP593M>
2019-06-07 15:11:42 -0300

    Servicio

--- src/poortravelling/Servicio.java
+++ src/poortravelling/Servicio.java
@@ -3,11 +3,9 @@ package poortravelling;
 
 
 public interface Servicio {
-    
   void setDisponibilidadTiempo(String disponibilidadDeTiempo);
   void setPrecio(Integer precio);
  
   String getDisponibilidadDeTiempo();
-  Integer getPrecio();
-  
+  Integer getPrecio();  
 }
